3b4f369082da20478953557b8cd8aa54beb5a612
Nicolas <nformenton@gmail.com>
2023-02-22 12:18:30 -0300

    input changes

--- weatherApp.py
+++ weatherApp.py
@@ -3,13 +3,26 @@ import json
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import config
+import datetime
 
 # Replace with your API keys
 opencage_api_key = config.opencage_api_key
 tomorrow_api_key = config.tomorrow_api_key
 
 # Input city name
-city_name = input("Enter a city name: ")
+city_name = input("Insira o nome de uma cidade: ")
+timesteps = input("Defina o período que quer ver, 1d ou 1h: ")
+
+# Get the current year
+current_year = datetime.datetime.now().year
+
+inital_date = input("Data e hora inicial (Ex: 22/02 9h): ")
+inital_parsed_date = datetime.datetime.strptime(inital_date, '%d/%m %Hh').replace(year=current_year)
+startTime = inital_parsed_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+final_date = input("Data e hora final (Ex: 22/02 11h): ")
+final_parsed_date = datetime.datetime.strptime(final_date, '%d/%m %Hh').replace(year=current_year)
+endTime = final_parsed_date.strftime('%Y-%m-%dT%H:%M:%SZ')
 
 # Get latitude and longitude coordinates for city using OpenCage API
 opencage_endpoint = f"https://api.opencagedata.com/geocode/v1/json?q={city_name}&key={opencage_api_key}"
@@ -26,26 +39,30 @@ longitude = opencage_data["results"][0]["geometry"]["lng"]
 # 1d, 1h
 fields = "temperature,windSpeed,humidity,precipitationProbability,precipitationIntensity,precipitationType,rainAccumulation,pressureSurfaceLevel,evapotranspiration"
 sampling = "1h"
-timesteps = "1d"
+# timesteps = "1d"
 
 tomorrow_endpoint = "https://api.tomorrow.io/v4/timelines?" \
                     "location={latitude},{longitude}" \
                     "&fields={fields}" \
                     "&sampling={sampling}" \
                     "&timesteps={timesteps}" \
-                    "&apikey={tomorrow_api_key}"
+                    "&apikey={tomorrow_api_key}" \
+                    "&startTime={startTime}" \
+                    "&endTime={endTime}"
 
 tomorrow_endpoint = tomorrow_endpoint.format(latitude=latitude,
                                              longitude=longitude,
                                              fields=fields,
                                              sampling=sampling,
                                              timesteps=timesteps,
-                                             tomorrow_api_key=tomorrow_api_key)
+                                             tomorrow_api_key=tomorrow_api_key,
+                                             startTime = startTime,
+                                             endTime = endTime)
 
 tomorrow_response = requests.get(tomorrow_endpoint)
 tomorrow_data = json.loads(tomorrow_response.text)
 
-with open('data.json', 'w') as f:
+with open('templates/data.json', 'w') as f:
     json.dump(tomorrow_data, f)
 
 with open('geo_data.json', 'w') as f:
@@ -93,34 +110,58 @@ for timeline in tomorrow_data["data"]["timelines"]:
         patm.append(interval["values"]["pressureSurfaceLevel"])
         ETp.append(interval['values']['evapotranspiration'])
 
-# Configuração de cores e estilos
-colors = ['deepskyblue', 'orangered', 'chartreuse', 'crimson', 'yellow', 'magenta']
-markers = ['circle', 'diamond', 'square', 'star', 'x']
+# Loop over the dates, parse them and format them
+formatted_dates = []
+for date in dates:
+    parsed_date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
+    formatted_date = parsed_date.strftime('%d/%m/%Y %Hh')
+    formatted_dates.append(formatted_date)
 
-# Configure subplots
-fig = make_subplots(rows=3, cols=2, subplot_titles=("Temperature (°C)", "Humidity (%)", "Wind Speed (Km/h)", "Precipitation Probability (%)", "Rain Accumulation (mm)", "ETp (mm/d)"))
+def show_values():
+    for i in range(len(dates)):
+        if timesteps == '1d':
+            metric = 'd'
+        if timesteps == '1h':
+            metric = 'h'
 
-# Add temperature trace
-fig.add_trace(go.Scatter(x=dates, y=temperatures, mode='lines+markers', name='Temperature', marker=dict(color=colors[0], symbol=markers[0])), row=1, col=1)
+        print(f'''
+        Data: {formatted_dates[i]}
+        Temperatura: {temperatures[i]} °C
+        Umidade: {humidities[i]} %
+        Velocidade do vento: {windspeeds_kmh[i]:.2f} km/h
+        Evapotranspiração: {ETp[i]} mm/{metric}
+        Possibilidade de precipitação: {pop[i]:.2f}%    
+        ''')
+show_values()
 
-# Add humidity trace
-fig.add_trace(go.Scatter(x=dates, y=humidities, mode='lines+markers', name='Humidity', marker=dict(color=colors[1], symbol=markers[0])), row=1, col=2)
+# # Configuração de cores e estilos
+# colors = ['deepskyblue', 'orangered', 'chartreuse', 'crimson', 'yellow', 'magenta']
+# markers = ['circle', 'diamond', 'square', 'star', 'x']
 
-# Add wind speed trace
-fig.add_trace(go.Scatter(x=dates, y=windspeeds_kmh, mode='lines+markers', name='Wind Speed', marker=dict(color=colors[2], symbol=markers[0])), row=2, col=1)
+# # Configure subplots
+# fig = make_subplots(rows=3, cols=2, subplot_titles=("Temperature (°C)", "Humidity (%)", "Wind Speed (Km/h)", "Precipitation Probability (%)", "Rain Accumulation (mm)", "ETp (mm)"))
 
-# Add precipitation probability trace
-fig.add_trace(go.Scatter(x=dates, y=pop, mode='lines+markers', name='Precipitation Probability', marker=dict(color=colors[3], symbol=markers[0])), row=2, col=2)
+# # Add temperature trace
+# fig.add_trace(go.Scatter(x=dates, y=temperatures, mode='lines+markers', name='Temperature', marker=dict(color=colors[0], symbol=markers[0])), row=1, col=1)
 
-# Add rain accumulation trace
-fig.add_trace(go.Scatter(x=dates, y=rain, mode='lines+markers', name='Rain Accumulation', marker=dict(color=colors[4], symbol=markers[0])), row=3, col=1)
+# # Add humidity trace
+# fig.add_trace(go.Scatter(x=dates, y=humidities, mode='lines+markers', name='Humidity', marker=dict(color=colors[1], symbol=markers[0])), row=1, col=2)
 
-# Add ETp
-fig.add_trace(go.Scatter(x=dates, y=ETp, mode='lines+markers', name='ETp', marker=dict(color=colors[5], symbol=markers[0])), row=3, col=2)
+# # Add wind speed trace
+# fig.add_trace(go.Scatter(x=dates, y=windspeeds_kmh, mode='lines+markers', name='Wind Speed', marker=dict(color=colors[2], symbol=markers[0])), row=2, col=1)
 
-# Update layout
-fig.update_layout(title=f"7-day Weather Forecast for {city_name}", height=1000, template='plotly_dark', hovermode="x unified", font=dict(family='Arial', size=12), xaxis=dict(tickangle=-45))
+# # Add precipitation probability trace
+# fig.add_trace(go.Scatter(x=dates, y=pop, mode='lines+markers', name='Precipitation Probability', marker=dict(color=colors[3], symbol=markers[0])), row=2, col=2)
 
-# Show plot
-fig.show()
+# # Add rain accumulation trace
+# fig.add_trace(go.Scatter(x=dates, y=rain, mode='lines+markers', name='Rain Accumulation', marker=dict(color=colors[4], symbol=markers[0])), row=3, col=1)
+
+# # Add ETp
+# fig.add_trace(go.Scatter(x=dates, y=ETp, mode='lines+markers', name='ETp', marker=dict(color=colors[5], symbol=markers[0])), row=3, col=2)
+
+# # Update layout
+# fig.update_layout(title=f"7-day Weather Forecast for {city_name}", height=1000, template='plotly_dark', hovermode="x unified", font=dict(family='Arial', size=12), xaxis=dict(tickangle=-45))
+
+# # Show plot
+# fig.show()
 
