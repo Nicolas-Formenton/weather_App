a8dbdda3f4592268452861f52b2dc57f1c9bb7b4
Nicolas <nformenton@gmail.com>
2023-03-01 20:38:58 -0300

    slight changes

--- frontEnd/src/Cafe.jsx
+++ frontEnd/src/Cafe.jsx
@@ -4,8 +4,6 @@ import Page2 from './page2';
 import { format } from 'date-fns';
 import Page3_2 from './Page3_2';
 import MenuBar from './menubar'
-import gado from './style/gado.css'
-
 function Page3() {
 
   const [cidade, setCidade] = useState('');
@@ -54,7 +52,7 @@ function Page3() {
         console.log(JSON.stringify(data));
         
         // enviando dados do formulário pra api
-        fetch('http://127.0.0.1:5000/dadosGado', {
+        fetch('http://127.0.0.1:5000/dadosCafe', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -88,17 +86,19 @@ const handleAddVisibilityRelatorio = () => {
    return <Page2 />;
  }else{
   return (
-    <form action = "/apiGado" method="post" className='divInput'  onSubmit={handleSubmit}>
+    <form action = "/apiCafe" method="post" className='divInput'  onSubmit={handleSubmit}>
         <button className='btnback' onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave} onClick={handleButtonBack}><svg className='svgarrowleft' viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><title/><path d="M10.1,23a1,1,0,0,0,0-1.41L5.5,17H29.05a1,1,0,0,0,0-2H5.53l4.57-4.57A1,1,0,0,0,8.68,9L2.32,15.37a.9.9,0,0,0,0,1.27L8.68,23A1,1,0,0,0,10.1,23Z"/></svg></button>
-        <input className='inputpage3' type="text" name="inputName" placeholder='NOME'/>
-        <input className='inputpage3' type="text" name="inputCabecas" placeholder='CABEÇAS DE GADO'/>
-        <input className='inputpage3' type="text" name="inputObs" placeholder='OBSERVAÇÃO'/>
+        <input className='inputpage3' type="text" name="inputName" placeholder='PRODUTO'/>
+        <input className='inputpage3' type="text" name="inputQuantidade" placeholder='QUANTIDADE'/>
+        <input className='inputpage3' type="text" name="inputDosagem" placeholder='DOSAGEM'/>
+        <input className='inputpage3' type="text" name="inputVelocidade" placeholder='VELOCIDADE DE APLICAÇÃO (KM/H)'/>
+
         <input className='inputpage3' type="text" name="inputCity" placeholder='CIDADE' value = {cidade} onChange={(e) => setCidade(e.target.value)}/>
         <div class='dates'>
-          <input className='inputpage3' type="text" name="inputDateInicial" placeholder="DD/MM HH" value = {dateEntrada} onChange={(e) => setDateEntrada(e.target.value)} />
+          <input className='inputpage3' type="text" name="inputDateInicial" placeholder="DIA/MES HORA" value = {dateEntrada} onChange={(e) => setDateEntrada(e.target.value)} />
           <div class='date_between'></div>
-          <input className='inputpage3' type="text" name="inputDateFinal" placeholder="DD/MM HH" value = {dateSaida} onChange={(e) => setDateSaida(e.target.value)}/>
+          <input className='inputpage3' type="text" name="inputDateFinal" placeholder="DIA/MES HORA" value = {dateSaida} onChange={(e) => setDateSaida(e.target.value)}/>
         </div>
         <button className='btnaddproduto' type='submit'  onMouseDown={handleMouseClickIn}
             onMouseUp={handleMouseClickOut}>SUBMIT</button>
