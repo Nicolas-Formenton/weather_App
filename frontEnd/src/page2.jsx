f6d1314177a530d31792081b663bb2cc36d8ef48
Gabriel <bielsantossk8@gmail.com>
2023-03-02 15:29:49 -0300

    Atualização ainda sobre menu no page2

--- frontEnd/src/page2.jsx
+++ frontEnd/src/page2.jsx
@@ -2,11 +2,11 @@ import { useState, useEffect } from 'react';
 import page2 from './style/page2.css'
 import Cafe from './Cafe'
 import Gado from './Gado'
-
+import MenuBar from './menubar';
 
 
 function Page2() {
-    //EFEITO CLICK PRODUTOS
+    //EFEITO CLICK CAFE
     const handleMouseClickIn = () => {
         const btnopcao = document.querySelector(".efeitoClick")
         btnopcao.style.transform = "translateY(3px)";
@@ -17,30 +17,7 @@ function Page2() {
         btnopcao.style.transform = "translateY(0px)";
       };
 
-    //EFEITO CLICK MUDAS
-      const handleMouseClickIn2 = () => {
-        const btnopcao2 = document.querySelector(".efeitoClick2")
-        btnopcao2.style.transform = "translateY(3px)";
-      };
-    
-      const handleMouseClickOut2 = () => {
-        const btnopcao2 = document.querySelector(".efeitoClick2")
-        btnopcao2.style.transform = "translateY(0px)";
-      };
-
-      //EFEITO CLICK PLANTIO
-
-      const handleMouseClickIn3 = () => {
-        const btnopcao3 = document.querySelector(".efeitoClick3")
-        btnopcao3.style.transform = "translateY(3px)";
-      };
-    
-      const handleMouseClickOut3 = () => {
-        const btnopcao3 = document.querySelector(".efeitoClick3")
-        btnopcao3.style.transform = "translateY(0px)";
-      };
-
-      //EFEITO CLICK COLHEITA
+      //EFEITO CLICK GADO
     
       const handleMouseClickIn4 = () => {
         const btnopcao4 = document.querySelector(".efeitoClick4")
@@ -62,6 +39,13 @@ function Page2() {
       const handleButtonGado = () => {
         setDestination("gado");
       };
+
+
+      //MenuBar barras
+      const exibirBarraSuperiorRelatorio = false;
+      const exibirBarraSuperiorCafe = false;
+      const exibirBarraSuperiorGado = false;
+      const exibirBarraSuperiorHome = true;
     
       if (destination === "cafe") {
         return <Cafe />;
@@ -76,6 +60,7 @@ function Page2() {
               
               <button className='btnopcao efeitoClick4' onMouseDown={handleMouseClickIn4}
                   onMouseUp={handleMouseClickOut4} onClick = {handleButtonGado}><svg height="200px" width="200px" version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" fill="white" stroke="white"><g id="SVGRepo_bgCarrier" stroke-whiteidth="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <style type="text/css"></style> <g> <path class="st0" d="M508.814,298.717c0,0-3.011-45.186-3.011-66.266c0-18.84-7.226-38.239-18.435-44.31 c-14.85-32.44-67.54-30.799-89.424-30.799c-25.605,0-82.032-1.538-106.88-1.538s-79.107-7.13-107.716-15.407 c-28.616-8.286-47.066-23.717-50.826-19.207c-3.761,4.524-11.297,9.798-21.836,8.285c21.836-8.285,20.33-31.619,17.32-34.63 c-3.012-3.012-15.065-3.768-26.354,9.783c6.772-12.802-6.031-30.113-12.802-33.881c-6.78-3.768-13.552-2.263-8.286,1.505 c5.274,3.769,8.286,20.331,0.757,27.111c0,0-23.342,14.3-27.858,17.319c-4.517,3.012-33.134,39.904-38.407,42.916 C9.79,162.608,0,167.874,0,170.894c0,3.012,11.296,25.971,17.319,25.971c6.023,0,53.081,5.648,82.446,7.911 c0,0,18.833,30.52,28.242,57.598c5.09,14.667,10.539,45.457,17.319,57.582c14.3,25.596,33.133,47.432,33.133,60.984 c0,13.559,0,43.672,0,43.672l-8.285,14.308c0,0,6.023,3.768,17.319,3.768c11.297,0,12.795-8.285,12.795-12.053 c0-3.761,0-56.467,0-56.467s-3.011-14.308-3.011-23.35c0-9.034,12.053-57.224,12.802-63.995c0,0,1.506,16.562,1.506,21.08 c0,0,35.1,12.165,53.089,13.957c30.114,3.011,51.201,9.042,94.874,4.517c35.323-3.649,63.294-24.497,63.294-24.497 s-3.02-14.308,0.749-21.836c6.022,42.167,40.661,71.532,40.661,73.786c0,2.263-5.313,70.058-5.313,70.058l-6.78,15.814 c0,0,2.302,3.736,13.599,3.736s15.057-6.779,15.057-15.057c0-8.285,15.057-79.068,15.814-84.334 c0.757-5.274-4.517-42.924-6.023-46.684c-1.179-2.956-0.048-51.895,0.502-76.407c6.509,21.884,7.282,57.756,5.665,68.719 c-3.386,22.959,6.022,45.178,6.022,45.178s3.012,7.528,7.529-9.034C514.837,309.257,508.814,298.717,508.814,298.717z"></path> </g> </g></svg>GADO</button>
+                  <MenuBar exibirBarraSuperiorRelatorio={exibirBarraSuperiorRelatorio} exibirBarraSuperiorCafe={exibirBarraSuperiorCafe} exibirBarraSuperiorGado={exibirBarraSuperiorGado} exibirBarraSuperiorHome={exibirBarraSuperiorHome}/>
           </div>
           );
       }
