b04f46a4cbf696965ff02110122ef4b31f4c7668
Gabriel <bielsantossk8@gmail.com>
2023-03-20 13:18:29 -0300

    Criando a pagina de Relatorios

--- frontEnd/src/Cafe2.jsx
+++ frontEnd/src/Cafe2.jsx
@@ -60,10 +60,14 @@ function ApiCafe() {
   }
 
   return (
-    <div>
+    <div className='divpai'>
       <button className='btnback' onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave} onClick={handleButtonBack}><svg className='svgarrowleft' viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><title/><path d="M10.1,23a1,1,0,0,0,0-1.41L5.5,17H29.05a1,1,0,0,0,0-2H5.53l4.57-4.57A1,1,0,0,0,8.68,9L2.32,15.37a.9.9,0,0,0,0,1.27L8.68,23A1,1,0,0,0,10.1,23Z"/></svg>
       </button>
+
+      <div className='semNome'>
+          <strong>ApiCafé</strong>
+      </div>
 
       {data.data.timelines.map((timeline, index) => (
         <div key={index}>
@@ -72,13 +76,13 @@ function ApiCafe() {
             {timeline.intervals.map((interval, index) => (
               <ul key={index}>
                 {/* <li>{FormatDate(interval.startTime)}</li> */}
-                <li>{interval.startTime}</li>
-                <li>Temperature: {interval.values.temperature}°C</li>
-                <li>Humidity: {interval.values.humidity}%</li>
-                <li>Wind speed: {interval.values.windSpeed} km/h</li>
-                <li>Evapotranspiration: {interval.values.evapotranspiration} mm/h</li>
-                <li>Precipitation probability: {interval.values.precipitationProbability}%</li>
-                <li>Rain accumulation: {interval.values.rainAccumulation} mm/h</li>
+                <li><strong>{interval.startTime}</strong></li>
+                <li>Temperature: <strong>{interval.values.temperature} °C</strong></li>
+                <li>Humidity: <strong>{interval.values.humidity} %</strong></li>
+                <li>Wind speed: <strong>{interval.values.windSpeed} km/h</strong></li>
+                <li>Evapotranspiration: <strong>{interval.values.evapotranspiration} mm/h</strong></li>
+                <li>Precipitation probability: <strong>{interval.values.precipitationProbability} %</strong></li>
+                <li>Rain accumulation: <strong>{interval.values.rainAccumulation} mm/h</strong></li>
               </ul>
             ))}
           </div>
